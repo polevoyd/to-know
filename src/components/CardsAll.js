4312b4ee119260aec507899382310b95da95e3d9
Dmitry <manxxeta@gmail.com>
2018-10-26 13:57:31 -0700

    initial commit

--- src/components/CardsAll.js
+++ src/components/CardsAll.js
@@ -4,7 +4,7 @@ class CardsAll extends React.Component {
     render(){
         return(
             <div>
-
+            
             </div>
         );
     }
---
+++ src/components/CardsFilterBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CardsFilterBar extends React.Component {
+    render(){
+        return(
+            <h1>Something</h1>
+        );
+    }
+}
+
+export default CardsFilterBar;
