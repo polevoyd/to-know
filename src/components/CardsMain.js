7b964e8b80d91626370dde39b98498c439b89584
Dmitry <manxxeta@gmail.com>
2018-10-26 15:17:13 -0700

    initial setup

--- src/components/CardsMain.js
+++ src/components/CardsMain.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import CardsFilterBar from './CardsFilterBar';
+import CardsResults from './CardsResults';
 
 class CardsMain extends React.Component {
     render(){
         return(
             <div>
                 <CardsFilterBar />
-                
+                <CardsResults />
             </div>
         );
     }
---
+++ src/components/CardsResults.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CardsResults extends React.Component {
+    render(){
+        return(
+            <div className="cards-results">
+
+            </div>
+        );
+    }
+}
+
+export default CardsResults;
