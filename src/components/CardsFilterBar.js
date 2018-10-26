348eac1d3586dd96ceb9babbd34806908054c70e
Dmitry <manxxeta@gmail.com>
2018-10-26 14:27:31 -0700

    initial commit

--- src/components/CardsFilterBar.js
+++ src/components/CardsFilterBar.js
@@ -5,8 +5,14 @@ class CardsFilterBar extends React.Component {
         return(
             <div className='filter-bar'>
                 <form>
-                    <input type="text" placeholder="find by keyword"></input>
+                    <input name="filterByKeyword" type="text" placeholder="find by keyword"></input>
                     <div><p>or use</p></div>
+                    <select name="filterBySelect">
+                        <option value="dataStructures">Concepts</option>
+                        <option value="dataStructures">Challanges</option>
+                        <option value="dataStructures">Data Structures</option>
+                        <option value="dataStructures">Algorithms</option>
+                    </select>
                 </form>
             </div>
         );
