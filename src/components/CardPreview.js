763043a1db474488e7517bd91f6025532f784f23
Dmitry <manxxeta@gmail.com>
2018-10-26 15:30:32 -0700

    initial setup

--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+class CardPreview extends React.Component {
+    render(){
+        return(
+            <div>
+                <p>Array</p>
+                <img src="https://via.placeholder.com/800"></img>
+            </div>
+        );
+    }
+}
+
+export default CardPreview;
--- src/components/CardsResults.js
+++ src/components/CardsResults.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CardPreview from './CardPreview';
 
 class CardsResults extends React.Component {
     render(){
         return(
             <div className="cards-results">
-
+                {/* here will be a logic for a loop of cards within selection on keyword */}
+                <CardPreview />
             </div>
         );
     }
