1b64d96f3c3cd53722c97ae4083340cdf32117af
Dmitry <manxxeta@gmail.com>
2018-10-29 15:13:30 -0700

    initial components

---
+++ src/components/CardForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CardForm extends React.Component{
+    render(){
+        return(
+            <div className="card-edit-form">
+                <h4>Card#</h4>
+                <p>Title</p>
+            </div>
+        );
+    }
+}
+
+export default CardForm;
