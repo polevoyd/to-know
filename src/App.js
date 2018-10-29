f3aa4539b84aa50d86880aa29e1923c37fb5078e
Dmitry <manxxeta@gmail.com>
2018-10-29 09:44:20 -0700

    starting

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 import CardsMain from './components/CardsMain';
 
 class App extends React.Component {
+
   render(){
+
+    /// here do a call for a api?
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+
+
+
     return(
       <div className="app">
         <CardsMain />
