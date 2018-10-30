600b50ab4cd94f65dc89f935aa1ed135c7631be5
Dmitry <manxxeta@gmail.com>
2018-10-30 09:37:12 -0700

    rdx

--- src/components/CardsResults.js
+++ src/components/CardsResults.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import CardForm from './CardForm';
 
 class CardsResults extends React.Component {
     constructor(props){
@@ -14,16 +13,18 @@ class CardsResults extends React.Component {
         }
     }
 
-    componentDidMount(){
-        // // calling api and get data
-        // fetch('https://jsonplaceholder.typicode.com/todos/')
-        // .then(response => response.json())
-        // .then(cards => this.setState({cards: cards}));
+
+    handleClick(name) {
+
+        // creating a link from name
+        const link = `https://repl.it/@polevoyd/${name.split(' ').join('')}?lite=true`;
+        const embeddedCodeEl = <iframe height="400px" width="100%" src={link} scrolling="no" frameborder="no" allowtransparency="yes" allowfullscreen="yes" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>;
+        console.log(embeddedCodeEl)
     }
 
     render(){
         const renderCards = this.state.cards.map(card => (
-            <div key={card.name} className="card-preview">
+            <div key={card.name} className="card-preview" onClick={() => this.handleClick(card.name)}>
                 <h4>{`${card.name}`}</h4>
                 <p>{`${card.keywords.map(word => word)} `}</p>
             </div>
