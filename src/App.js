5ceccbb6b82c2df37811df25c950d171eb3d5bff
Dmitry <manxxeta@gmail.com>
2018-10-31 09:01:52 -0700

    rdx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import CardsMain from './components/CardsMain';
 
-// import
-// createStore - to initialise store
-// provider - to provide store for all children in tree
-// cardReducer - reducer function
-import {  createStore   } from 'redux';
-import {  Provider      } from 'react-redux';
-import    cardReducer   from './reducers/cardReducer';
 
-// initialising a store with reducer function
-const store = createStore(cardReducer);
 
 class App extends React.Component {
 
   render(){
     return(
       <div className="app">
-      {/* wrapping in provider to make available for children  */}
-      <Provider store={store}>
         <CardsMain />
-      </Provider>
       </div>
     );
   }
--- src/components/CardsMain.js
+++ src/components/CardsMain.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import {  Provider      } from 'react-redux';
+
 import { connect } from 'react-redux';
 
 import CardsFilterBar from './CardsFilterBar';
