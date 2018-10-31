33bd98c62aebc8f050f66042870a8215f95a4372
Dmitry <manxxeta@gmail.com>
2018-10-30 20:25:02 -0700

    rdx

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -6,7 +6,7 @@ class CardForm extends React.Component{
 
         // creating a link from name
         const link = `https://repl.it/@polevoyd/${this.props.cardToShow.split(' ').join('')}?lite=true`;
-        const embeddedCodeEl = <iframe height="500px" width="100%" src={link} scrolling="no" frameBorder="no" allowtransparency="yes" allowFullScreen="yes" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals">
+        const embeddedCodeEl = <iframe height="700px" width="100%" src={link} scrolling="no" frameBorder="no" allowtransparency="yes" allowFullScreen="yes" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals">
         
         </iframe>;
  
@@ -15,7 +15,6 @@ class CardForm extends React.Component{
                 <div className="card-edit-form-header">
                     <h4>{this.props.cardToShow}</h4>
                     <div className="card-form button close" onClick={this.props.handlePanelClicks}></div>
-                    <div className="card-form button edit" onClick={this.props.handlePanelClicks}></div>
                     <div className="card-form button delete" onClick={this.props.handlePanelClicks}></div>
                 </div>
                 {embeddedCodeEl}
