1af27290598b2609a6013ec016c23fb2fc6d5058
Dmitry <manxxeta@gmail.com>
2018-11-01 14:51:21 -0700

    rdx

--- src/components/CardsMain.js
+++ src/components/CardsMain.js
@@ -1,11 +1,9 @@
 import React from 'react';
-// import CardsFilterBar from './CardsFilterBar';
 import CardsResults from './CardsResults';
 import AddCards from './AddCards';
 
 const CardsMain = () => (
     <div>
-        {/* <CardsFilterBar /> */}
         <AddCards />
         <CardsResults />
     </div>
