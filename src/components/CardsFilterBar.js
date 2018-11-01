799aac15c794d9b9a78acd361f9d9b8c87828987
Dmitry <manxxeta@gmail.com>
2018-11-01 12:29:59 -0700

    rdx

--- src/components/CardsFilterBar.js
+++ src/components/CardsFilterBar.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
 class CardsFilterBar extends React.Component {
+
+    handleFilterClick(event){
+        event.preventDefault();
+
+    }
+
     render(){
         return(
             <div className='filter-bar'>
                 <form>
                     <input name="filterByKeyword" type="text" placeholder="find by keyword"></input>
                     <div className="categories-filters">
-                        <button className="group-one"></button>
-                        <button className="group-two"></button>
-                        <button className="group-three"></button>
+                        <button onClick={this.handleFilterClick} className="group-one"></button>
+                        <button onClick={this.handleFilterClick} className="group-two"></button>
+                        <button onClick={this.handleFilterClick} className="group-three"></button>
                     </div>
                 </form>
 
