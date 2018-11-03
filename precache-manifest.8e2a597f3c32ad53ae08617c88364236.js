d76d85d4bd8df3336c4f5a7acdee440081e0316b
Dmitry <manxxeta@gmail.com>
2018-11-02 17:52:09 -0700

    Updates

---
+++ precache-manifest.8e2a597f3c32ad53ae08617c88364236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6845a5252c53886f2c9",
+    "url": "/to-know/static/js/runtime~main.d6845a52.js"
+  },
+  {
+    "revision": "17b859e8c5dfdad170c5",
+    "url": "/to-know/static/js/main.17b859e8.chunk.js"
+  },
+  {
+    "revision": "d9c82340347c27601e28",
+    "url": "/to-know/static/js/1.d9c82340.chunk.js"
+  },
+  {
+    "revision": "17b859e8c5dfdad170c5",
+    "url": "/to-know/static/css/main.760c4551.chunk.css"
+  },
+  {
+    "revision": "a2c199d81fe2da13a302dcd25cc03ad1",
+    "url": "/to-know/index.html"
+  }
+];
