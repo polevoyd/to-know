2ca63ef543ed351ecbad1ee1f09b75aef9df02d4
Dmitry <manxxeta@gmail.com>
2018-11-04 12:31:14 -0800

    ui improvements

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -20,14 +20,18 @@ class CardForm extends React.Component{
         const embeddedCodeEl = <iframe title="currentCard" height="700px" width="100%" src={link} scrolling="no" frameBorder="no" allowtransparency="yes" allowFullScreen="yes" sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"></iframe>;
 
         // LINK TO REQUEST NAMES AND LINKS: https://to-know.herokuapp.com/
-
+        // https://raw.githubusercontent.com/polevoyd/js-challenges/master/Misc:%20Candies.js
+        
+        const el = <div>
+            <object src="https://raw.githubusercontent.com/polevoyd/js-challenges/master/Misc:%20Candies.js"></object>
+        </div>
         return(
             <div className="card-edit-form">
                 <div className="card-edit-form-header">
                     <h4>{this.props.cardToShow}</h4>
                     <div className="card-form button close" onClick={this.props.handlePanelClicks}></div>
                 </div>
-                {embeddedCodeEl}
+                {el}
             </div>
         );
     }
