b413b956b5c3612caa6f6b7c9d19b216d137eec3
Dmitry <manxxeta@gmail.com>
2018-11-08 11:35:55 -0800

    gh ref

--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -3,6 +3,20 @@ import React from 'react';
 class CardPreview extends React.Component {
 
     /***********************************************************/
+    // Returns random color
+
+    randomizeColor() {
+
+
+        // 'rgba(159, 211, 86, 1)'
+
+        return `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1)`;
+        
+    }
+
+
+
+    /***********************************************************/
     // Return a class name depending on category
 
     pickClass(category) {
@@ -26,6 +40,15 @@ class CardPreview extends React.Component {
 
     render() {
 
+        // will take color from a state
+        const customStyleFromState = {
+            // color: ,
+            // backgroundColor: 
+        }
+
+
+
+        
         // filter cards by specific category
         const cards = this.props.cards;
         const categoryToFilter = this.props.category;
@@ -34,6 +57,7 @@ class CardPreview extends React.Component {
         .filter(cardObj => cardObj.category === categoryToFilter)
         .map(card => {
             return(<div key={categoryToFilter + card.name} 
+                style={customStyleFromState}
                 className={this.pickClass(this.props.category)} 
                 onClick={() => this.props.handleClick(card.name)}
                 onDragStart={(event) => this.props.onDragStart(event, card.name)}
