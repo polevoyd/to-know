266ab4b5d806dc804cf4fc911293615c98e0b7db
Dmitry <manxxeta@gmail.com>
2018-11-09 12:42:27 -0800

    colors editor

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -5,7 +5,8 @@ const Greeting = (props) => {
         <div className="greeting">
             <h1>To Know:</h1>
             <p>Hello!</p>
-            <p>This is a organiser interface for a GitHub repository. Every file from a repo will be shown as a card, which you can open and drag and drop across three different categories. It makes comfortable to study concepts and code challanges, while making managing them in one place effortless. Hope you'll find it helpful for yourself too!</p>
+            <p>This is an app that convert your GitHub repository into a study cards. Every file from a repo as a card, which you can open and drag and drop across three different categories. It makes it easy to study concepts and code challanges, while effortlessly managing them in one place. Hope you'll find it helpful too!</p>
+            <h5>Press a menu icon in top left corner to start</h5>
         </div>
     );
 }
